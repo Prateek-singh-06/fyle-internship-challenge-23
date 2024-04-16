fbf9ddc530a817399ff4ff024effe60752fd3769
PRATEEK KUMAR SINGH <prateek99555@gmail.com>
2024-04-17 04:15:12 +0530

    bug fixed

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -19,9 +19,9 @@ export class ApiService {
 
   getUser(githubUsername: string|null) {
     this.url="https://api.github.com/users/"+githubUsername;
-    const headers = { 'Authorization': 'Bearer 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'};
+    // const headers = { 'Authorization': 'Bearer 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'};
     console.log(githubUsername);
-    return this.httpClient.get(this.url,{headers});
+    return this.httpClient.get(this.url);
   }
   changeUserData(data: any) {
     this.userData.next(data);
@@ -30,13 +30,13 @@ export class ApiService {
 
   getrepo(repoPerPage:number|null,pageNO:number|null){
     // console.log("called");
-    const headers = { 'Authorization': 'Bearer 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'};
+    // const headers = { 'Authorization': 'Bearer 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'};
     const cachedResponse = this.cache.get(`${this.url}/repos?per_page=${repoPerPage}&page=${pageNO}`);
     if(cachedResponse)
       {
         return of(cachedResponse);
       }
-    return this.httpClient.get(`${this.url}/repos?per_page=${repoPerPage}&page=${pageNO}`,{headers}).pipe(tap(response=>this.cache.set(`${this.url}/repos?per_page=${repoPerPage}&page=${pageNO}`,response)));
+    return this.httpClient.get(`${this.url}/repos?per_page=${repoPerPage}&page=${pageNO}`).pipe(tap(response=>this.cache.set(`${this.url}/repos?per_page=${repoPerPage}&page=${pageNO}`,response)));
   }
   changeRepo(data:any){
     this.repos.next(data);
